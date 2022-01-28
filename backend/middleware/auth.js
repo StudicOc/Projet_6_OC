012059d8ef0b372ca7692f74283e5840050cd7b1
StudicOC <sabrinaleb48@gmail.com>
2022-01-28 11:38:03 +0100

    Vérification Token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,13 +10,13 @@ module.exports = (req, res, next) => {
     const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
-      throw "Invalid user ID";
+      throw "User Id non valable";
     } else {
       next();
     }
   } catch {
     res.status(401).json({
-      error: new Error("Invalid request!"),
+      error: new Error("Requête non authentifié!"),
     });
   }
 };
